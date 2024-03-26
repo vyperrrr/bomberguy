9d09bfb59aa9307bde83216c8a4de4372a25b9c3
Gerzsenyi Patrik <78800814+vyperrrr@users.noreply.github.com>
2024-03-26 22:24:20 +0100

    feat : If the explosion hits a crate it stops spreading in the crate's direction afterwards

--- core/src/s11/bomberguy/explosives/Explosion.java
+++ core/src/s11/bomberguy/explosives/Explosion.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Timer;
 import s11.bomberguy.Collidables;
 import s11.bomberguy.mapElements.Wall;
+import s11.bomberguy.mapElements.Crate;
 
 import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -63,15 +64,20 @@ public class Explosion extends Sprite {
             // Spread in all directions
             if (direction == 0) {
                 directions.forEach(direction -> {
-                    if(canSpreadInDirection(direction[0],direction[1]))
-                        explosionsToAdd.add(new Explosion(direction[0], direction[1], directions.indexOf(direction) + 1, this.range - 1));
+                    boolean[] flags = filter(direction[0],direction[1]);
+                    // If the next explosion is going to hit a crate don't spread after wards
+                    int nextRange = flags[1] ? 0 : this.range-1;
+                    if(!flags[0])
+                        explosionsToAdd.add(new Explosion(direction[0], direction[1], directions.indexOf(direction) + 1, nextRange));
                 });
             } else
             // Spread in one direction
             {
                 float directionX = directions.get(this.direction - 1)[0];
                 float directionY = directions.get(this.direction - 1)[1];
-                if(canSpreadInDirection(directionX,directionY))
+                boolean[] flags = filter(directionX,directionY);
+
+                if(!flags[0])
                     explosionsToAdd.add(new Explosion(directions.get(this.direction - 1)[0], directions.get(this.direction - 1)[1], 1, this.range - 1));
 
             }
@@ -80,14 +86,21 @@ public class Explosion extends Sprite {
         }
     }
 
-    private boolean canSpreadInDirection(float directionX, float directionY)
+    // WALL_HIT_FLAG - next explosion will hit a wall if true
+    // CRATE_HIT_FLAG - next explosion will hit a crate if true
+    private boolean[] filter(float directionX, float directionY)
     {
-        AtomicBoolean SAFE_ROUTE_FLAG = new AtomicBoolean(true);
+        AtomicBoolean WALL_HIT_FLAG = new AtomicBoolean(false);
+        AtomicBoolean CRATE_HIT_FLAG = new AtomicBoolean(false);
+
         Collidables.getInstance().getCollidables().forEach(collidable -> {
             if(collidable.getX() == directionX && collidable.getY() == directionY && collidable instanceof Wall)
-                SAFE_ROUTE_FLAG.set(false);
+                WALL_HIT_FLAG.set(true);
+            if(collidable.getX() == directionX && collidable.getY() == directionY && collidable instanceof Crate)
+                CRATE_HIT_FLAG.set(true);
         });
-        return  SAFE_ROUTE_FLAG.get();
+
+        return new boolean[]{WALL_HIT_FLAG.get(), CRATE_HIT_FLAG.get()};
     }
 
     private void stop() {
