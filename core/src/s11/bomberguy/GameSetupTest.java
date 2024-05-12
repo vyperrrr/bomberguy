ce209a0fa99b7f09df165058ccc8467eb5da139b
Barabás Balázs <daiiwj@inf.elte.hu>
2024-05-13 00:41:47 +0200

    Feat: GameSetup test updated

---
+++ core/src/s11/bomberguy/GameSetupTest.java
@@ -0,0 +1,37 @@
+package s11.bomberguy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameSetupTest {
+    @Test
+    public void testSetAndGetPlayerNum() {
+        int initialPlayerNum = GameSetup.getPlayerNum();
+        GameSetup.setPlayerNum(3);
+        assertAll(
+                () -> assertEquals(2, initialPlayerNum, "Player number should be 2"),
+                () -> assertEquals(3, GameSetup.getPlayerNum(), "Player number should be 3")
+        );
+    }
+
+    @Test
+    public void testSetAndGetMapNum() {
+        int initialMapNum = GameSetup.getMapNum();
+        GameSetup.setMapNum(2);
+        assertAll(
+                () -> assertEquals(1, initialMapNum, "Map number should be 1"),
+                () -> assertEquals(2, GameSetup.getMapNum(), "Map number should be 2")
+        );
+    }
+
+    @Test
+    public void testSetAndGetRounds() {
+        int initialRoundNum = GameSetup.getRounds();
+        GameSetup.setRounds(2);
+        assertAll(
+                () -> assertEquals(1, initialRoundNum, "Round number should be 1"),
+                () -> assertEquals(2, GameSetup.getRounds(), "Round number should be 2")
+        );
+    }
+}
